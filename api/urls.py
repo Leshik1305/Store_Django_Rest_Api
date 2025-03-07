7d88b0a731862a6863f827e8e5f5569bbff28028
Leshik1305 <realleshik1305@gmail.com>
2025-03-07 21:15:25 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import CustomerUserModelViewSet, StorehouseModelViewSet, ProductModelViewSet, SupplyModelViewSet, ConsumptionModelViewSet
 
-router = DefaultRouter()
-router.register('users', CustomerUserModelViewSet)
-router.register('storehouses', StorehouseModelViewSet)
-router.register('products', ProductModelViewSet)
-router.register('supplies', SupplyModelViewSet)
-router.register('consumptions', ConsumptionModelViewSet)
+router = DefaultRouter()  # Создание маршрутизатора
+
+router.register('users', CustomerUserModelViewSet)  # Регистрация представления для пользователей
+router.register('storehouses', StorehouseModelViewSet)   # Регистрация представления для товаров
+router.register('products', ProductModelViewSet)  # Регистрация представления для складов
+router.register('supplies', SupplyModelViewSet)  # Регистрация представления для поставок
+router.register('consumptions', ConsumptionModelViewSet)   # Регистрация представления для потреблений
 
 urlpatterns = [
 
