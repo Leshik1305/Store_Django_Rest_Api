1fcf2360981e6c56c97e33c1cad49788f7b54987
Leshik1305 <realleshik1305@gmail.com>
2025-03-25 14:56:09 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,18 +2,24 @@ from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 from api.views import CustomerUserModelViewSet, StorehouseModelViewSet, ProductModelViewSet, SupplyModelViewSet, ConsumptionModelViewSet
 
-router = DefaultRouter()  # Создание маршрутизатора
+router = DefaultRouter()
+''' Создание маршрутизатора '''
 
-router.register('users', CustomerUserModelViewSet)  # Регистрация представления для пользователей
-router.register('storehouses', StorehouseModelViewSet)   # Регистрация представления для товаров
-router.register('products', ProductModelViewSet)  # Регистрация представления для складов
-router.register('supplies', SupplyModelViewSet)  # Регистрация представления для поставок
-router.register('consumptions', ConsumptionModelViewSet)   # Регистрация представления для потреблений
+router.register('users', CustomerUserModelViewSet)
+''' Регистрация представления для пользователей '''
+router.register('storehouses', StorehouseModelViewSet)
+''' Регистрация представления для товаров '''
+router.register('products', ProductModelViewSet)
+''' Регистрация представления для складов '''
+router.register('supplies', SupplyModelViewSet)
+''' Регистрация представления для поставок '''
+router.register('consumptions', ConsumptionModelViewSet)
+''' Регистрация представления для потреблений '''
 
 
 urlpatterns = [
-    path("", include(router.urls)), #включаем маршруты, которые зарегестрировали выше
-    path('api-auth/', include('rest_framework.urls')), #Адрес для аунтефикации
+    path("", include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns.extend(router.urls)
