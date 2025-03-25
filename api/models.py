2d7254a8931a4f0066e96e31b3a53f003c1806cc
Leshik1305 <realleshik1305@gmail.com>
2025-03-25 14:57:00 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,34 +1,39 @@
-#Импорт моделей из Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 
-class CustomUser(AbstractUser):  #Пользовательская модель, расширяющая стандартную модель пользователя Django
+class CustomUser(AbstractUser):
+    '''Пользовательская модель, расширяющая стандартную модель пользователя Django'''
 
     ROLE_CHOICES = [
-        ("supplier", "Supplier"), # Значение для поставщика
-        ("consumer", "Consumer"), # Значение для потребителя
+        ("supplier", "Supplier"),
+        ("consumer", "Consumer"),
     ]
-    role = models.CharField(max_length=15,  choices=ROLE_CHOICES)  # Поле для хранения типа пользователя
+    ''' выбор вида юзера (поставщик и потребитель) '''
+    role = models.CharField(max_length=15,  choices=ROLE_CHOICES)
 
 
 
-class Storehouse(models.Model): # Модель для представления склада
-    name = models.CharField(max_length=128)  # Название склада
+class Storehouse(models.Model):
+    ''' Модель для представления склада'''
+    name = models.CharField(max_length=128)
 
 
-class Product(models.Model): # Модель для товара
-    name = models.CharField(max_length=128) # Название товара
-    quantity = models.PositiveIntegerField()  # Количесвто товара
-    storehouse = models.ForeignKey(Storehouse, related_name="products", on_delete=models.CASCADE) # Внешний ключ на склад, связь многие к одному
+class Product(models.Model):
+    ''' Модель для товара '''
+    name = models.CharField(max_length=128)
+    quantity = models.PositiveIntegerField()
+    storehouse = models.ForeignKey(Storehouse, related_name="products", on_delete=models.CASCADE)
 
-class Supply(models.Model):  # Модель для представления поставок товара
-    supplier = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Внешний ключ на поставщика
-    product =  models.ForeignKey(Product, on_delete=models.CASCADE)   # Внешний ключ на товар
-    quantity = models.PositiveIntegerField()  # Количесвто товара
+class Supply(models.Model):
+    ''' Модель для представления поставок товара '''
+    supplier = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name = 'supplier')
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
 
 class Consumption(models.Model):
-    consumer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)   # Внешний ключ на потребителя
-    product =  models.ForeignKey(Product, on_delete=models.CASCADE)   # Внешний ключ на товар
-    quantity = models.PositiveIntegerField()  # Количесвто товара
+    ''' Модель для представления потребления товара '''
+    consumer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name = 'consumer' )
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
